ca46ced39b8b1b972ac4fce0a89e1f4d21739f00
f1ames <dr.odpowiedz@gmail.com>
2023-05-18 16:22:22 +0200

    feat: use Monokle validation in webhook

--- kac-api/src/index.ts
+++ kac-api/src/index.ts
@@ -2,10 +2,13 @@ import {readFileSync} from "fs";
 import path from "path";
 import fastify from "fastify";
 import {V1ValidatingWebhookConfiguration, V1ObjectMeta} from "@kubernetes/client-node";
+import {createDefaultMonokleValidator, Resource} from "@monokle/validation";
 
-// For some reason the type definitions for the request body has 'metadata' filed instead of 'request'.
+// TODO - some mismatch with types, this is not exactly the type which should be used
 type AdmissionRequest = V1ValidatingWebhookConfiguration & {
-    request?: V1ObjectMeta
+    request?: V1ObjectMeta & {
+      object?: Resource
+    }
 }
 
 type AdmissionResponse = {
@@ -19,6 +22,8 @@ type AdmissionResponse = {
         }
     }
 }
+
+const validator = createDefaultMonokleValidator();
 
 const server = fastify({
   https: {
@@ -37,26 +42,87 @@ server.post("/validate", async (req, res): Promise<AdmissionResponse> => {
     apiVersion: body?.apiVersion || '',
     response: {
       uid: body?.request?.uid || "",
-      // allowed: false,
       allowed: true,
       status: {
-        // message: "You shall not pass!"
-        message: "You shall pass!"
+        message: "OK"
       }
     }
   }
 
-  console.log('response', response);
+  // Dev workaround - always return true for webhok server to not block hot-reload
+  if (body.request?.name?.startsWith('webhook-server-')) {
+    console.log('Allowing webhook server to pass', response);
+
+    return response;
+  }
+
+  await validator.preload({
+    plugins: {
+      "kubernetes-schema": true,
+      "open-policy-agent": true,
+    },
+  });
+
+  const resourceForValidation = createResourceForValidation(body);
+  const validationResponse = await validator.validate({ resources: [resourceForValidation] });
+
+  const warnings = [];
+  const errors = [];
+  for (const result of validationResponse.runs) {
+    for (const item of result.results) {
+      if (item.level === "warning") {
+        warnings.push(item);
+      } else if (item.level === "error") {
+        errors.push(item);
+      }
+    }
+  }
+
+  if (errors.length > 0 || warnings.length > 0) {
+    const warningsList = warnings.map((e) => `${e.ruleId}: ${e.message.text}`).join("\n");
+    const errorsList = errors.map((e) => `${e.ruleId}: ${e.message.text}`).join("\n");
+    const message = [];
+
+    if (errors.length > 0) {
+      message.push(`\n${errors.length} errors found:\n${errorsList}`);
+    }
+
+    if (warnings.length > 0) {
+      message.push(`\n${warnings.length} warnings found:\n${warningsList}`);
+    }
+
+    message.push("\n\nYou can use Monokle (https://monokle.io/) to validate and fix errors easily!");
+
+    response.response.allowed = false;
+    response.response.status.message = message.join("");
+  }
+
+  console.log('response', resourceForValidation, validationResponse, response);
 
   return response;
 });
 
-const PORT = 8443;
-
-server.listen({port: PORT, host: '0.0.0.0' }, (err, address) => {
+server.listen({port: 8443, host: '0.0.0.0' }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
   }
   console.log(`Server listening at ${address}`);
 });
+
+function createResourceForValidation(admissionResource: AdmissionRequest): Resource {
+  const resource = {
+    id: admissionResource.request?.uid || '',
+    fileId: '',
+    filePath: '',
+    fileOffset: 0,
+    name: admissionResource.request?.name || '',
+    apiVersion:  admissionResource.request?.object?.apiVersion || '',
+    kind: admissionResource.request?.object?.kind || '',
+    namespace: admissionResource.request?.namespace || '',
+    content: admissionResource.request?.object || {},
+    text: ''
+  };
+
+  return resource;
+}
