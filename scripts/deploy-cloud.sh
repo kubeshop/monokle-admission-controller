7d510cde2fbe2bf73516dfb0a02823eff4928e3c
f1ames <dr.odpowiedz@gmail.com>
2023-10-27 14:45:41 +0200

    chore: rework deploy script to pass API URL

--- scripts/deploy-cloud.sh
+++ scripts/deploy-cloud.sh
@@ -5,13 +5,19 @@
 set -euo pipefail
 
 basedir="$(dirname "$0")"
+cloudApiUrl="$1"
 
 # Generate install.yaml
+#
+# For local testing:
+# cloudApiUrl=host.docker.internal:5000 // When running minikube via Docker Desktop (e.g. WSL).
+# cloudApiUrl=host.minikube.internal:5000 // All other usages.
 helm template "${basedir}/../helm" \
 --set image.init.overridePath=admission-webhook-init \
 --set image.server.overridePath=admission-webhook \
 --set image.synchronizer.overridePath=admission-synchronizer \
 --set logLevel=debug \
+--set cloudApiUrl=$cloudApiUrl \
 --set automationToken=SAMPLE_TOKEN > "${basedir}/install.yaml"
 
 # Run deployment through skaffold with locally build images
